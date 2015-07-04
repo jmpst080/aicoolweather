62a5108c0fde234910f864171db092c11d024279
jmpst080 <364566690@qq.com>
2015-07-04 14:31:48 +0800

    First commit

---
+++ src/com/aicoolweather/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.aicoolweather.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		String str="https://github.com/jmpst080/aicoolweather.git";
+	}
+}
